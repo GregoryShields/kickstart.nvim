fb335cdba96e9cddf158f79e81e8af68396b5c7e
Laptop Mac <gs@gregoryshields.net>
2024-01-10 04:28:12 -0800

    Update init.lua to load my vimrc and removed crufty comments.

--- init.lua
+++ init.lua
@@ -1,48 +1,22 @@
+-- See: $VIMWIKI/Software/Tools/Vim/Options/set.md
+vim.api.nvim_command('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
+vim.api.nvim_command('let &packpath = &runtimepath')
+vim.api.nvim_command('source ~/.vim/vimrc')
+
+--do
+--  return
+--end
+
 --[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
   - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
   - https://neovim.io/doc/user/lua-guide.html
 
+There are several `:help X` comments throughout the init.lua file.
+Run that command and read that Help section for more information.
 
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
+In addition, I there are some `NOTE:` items throughout the file.
+These are for you to help understand what is happening.
 --]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
@@ -656,6 +630,12 @@ cmp.setup {
     { name = 'path' },
   },
 }
+
+-- Yuki vimpad mappings from init.vim (adapt them for init.lua)
+--nmap <leader>oo <Plug>(vimpad-on)
+--nmap <leader>of <Plug>(vimpad-off)
+--nmap <leader>ot <Plug>(vimpad-toggle)
+--nmap <leader>or <Plug>(vimpad-refresh)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
